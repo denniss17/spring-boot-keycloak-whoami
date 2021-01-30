b7dbcc646d534a6011ec57e7400ca83124333c57
Dennis Schroer <dennis.schroer@topicus.nl>
2021-01-30 21:59:02 +0100

    Initial commit

---
+++ src/main/java/nl/dennisschroer/springbootkeycloakwhoami/SpringBootKeycloakWhoamiApplication.java
@@ -0,0 +1,13 @@
+package nl.dennisschroer.springbootkeycloakwhoami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloakWhoamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKeycloakWhoamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/dennisschroer/springbootkeycloakwhoami/SpringBootKeycloakWhoamiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.dennisschroer.springbootkeycloakwhoami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakWhoamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
